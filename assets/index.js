e6f59d463e63996182f182eb3d0fce6ee61e58e6
Jaime M <jaimemarsh19@gmail.com>
2023-03-27 10:38:21 -0400

    readme function, error still there

--- assets/index.js
+++ assets/index.js
@@ -62,88 +62,42 @@ console.log(response);
     })
 })
 console.log(inquirer)
-// // TODO: Create a function to initialize app
-// function init() { }
 
-// // Function call to initialize app
-// init();
+const generateREADME = function
+    (title, description, installation, usage, contribution, tests, license, github, email) {
+    return `
+        # ${title}
+        ##Table of Contents
+        [Installation](#installation)
+        [Usage](#usage)
+        [Contribution](#contribution)
+        [Tests](#tests)
+        [License](#license)
+        [Questions](#questions)
 
+        ##Discription
+        ${description}
 
+        ##Installation
+        ${installation}
 
+        ##Usage
+        ${usage}
 
+        ##Contribution
+        ${contribution}
 
+        ##Tests
+        ${tests}
 
+        ##License
+        ${license}
 
+        ##Questions
+        If you have any questions about the repo, open an issue or contact me directly at ${email}.
+        You can find more of my work at my [GitHub](https://github.com/${github})
 
-
-//class example
-const generateHTML = function (name, location, bio, linkedin, github) {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    
-    <body>
-        <header>
-            <div>
-                <h1>Hi! My name is ${name}</h1>
-                <p>Bio: ${bio}</p>
-                <p>I am from ${location}</p>
-                <p>My github username is ${github}</p>
-                <p>My linkedin account is ${linkedin}</p>
-            </div>
-        </header>
-    </body>
-    
-    </html>`;
+        `
 }
-
-
-
-    // const inquirer = require('inquirer');
-
-    // inquirer.prompt([
-    //     {
-    //         type: 'name',
-    //         message: 'What is your name?',
-    //         name: 'name',
-    //     },
-    //     {
-    //         type: 'location',
-    //         message: 'What is your location?',
-    //         name: 'location',
-    //     },
-    //     {
-    //         type: 'bio',
-    //         message: 'Tell us about yourself',
-    //         name: 'bio',
-    //     },
-    //     {
-    //         type: 'linkedin',
-    //         message: 'What is your linkedin profile?',
-    //         name: 'linkedin',
-    //     },
-    //     {
-    //         type: 'github',
-    //         message: 'What is your github profile?',
-    //         name: 'github',
-    //     },
-    // ])
-//     .then((response) => {
-//         console.log(response);
-//         const fs = require('fs');
-//         const htmlPageContent = generateHTML(response.name, response.location, response.bio, response.linkedin, response.github);
-
-//         fs.writeFile('index.html', htmlPageContent, (err) => {
-//             err ? console.error(err) : console.log("Created index.html")
-//         })
-//     })
-
-// console.log(inquirer)
 
 
