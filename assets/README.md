0830f5a92797c0b4b7acb2fd222d22d16391baa9
Jaime M <jaimemarsh19@gmail.com>
2023-03-28 21:58:40 -0400

    TA helped with error

---
+++ assets/README.md
@@ -0,0 +1,33 @@
+
+        # title
+        ##Table of Contents
+        [Installation](#installation)
+        [Usage](#usage)
+        [Contribution](#contribution)
+        [Tests](#tests)
+        [License](#license)
+        [Questions](#questions)
+
+        ##Discription
+        hello
+
+        ##Installation
+        test
+
+        ##Usage
+        test
+
+        ##Contribution
+        test
+
+        ##Tests
+        test
+
+        ##License
+        MIT
+
+        ##Questions
+        If you have any questions about the repo, open an issue or contact me directly at test.
+        You can find more of my work at my [GitHub](https://github.com/test)
+
+        
--- assets/index.js
+++ assets/index.js
@@ -3,70 +3,62 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'What is your projects title?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'Give a brief description of your project.',
-        name: 'description',
-    },
-    {
-        type: 'input',
-        message: 'What are your installation instructions?',
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: 'Provide usage information.',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines?',
-        name: 'contribution',
-    },
-    {
-        type: 'input',
-        message: 'What tests were performed?',
-        name: 'tests',
-    },
-    {
-        type: 'list',
-        message: 'What license would you like to use?',
-        choices: ["MIT", "Apache", "GNU", "BSD 2"],
-        name: 'license',
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'github',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
-])
-console.log(response);
+function prompts() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your projects title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Give a brief description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are your installation instructions?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Provide usage information.',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What are the contribution guidelines?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'What tests were performed?',
+            name: 'tests',
+        },
+        {
+            type: 'list',
+            message: 'What license would you like to use?',
+            choices: ["MIT", "Apache", "GNU", "BSD 2"],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+    ])
+}
 
-// TODO: Create a function to write README file
-.then((response) => {
-    const readmePageContent = generateREADME(response);
-    // response.title, response.location, response.description, response.installation, response.usage, response.contribution, response.tests, response.license, response.github, response.email
-    fs.writeFile('README.md', readmePageContent, (err) => {
-        err ? console.error(err) : console.log("Created README.md")
-    })
-})
-console.log(inquirer)
 
 const generateREADME = function
-    (title, description, installation, usage, contribution, tests, license, github, email) {
+    (response) {
     return `
-        # ${title}
+        # ${response.title}
         ##Table of Contents
         [Installation](#installation)
         [Usage](#usage)
@@ -76,28 +68,40 @@ const generateREADME = function
         [Questions](#questions)
 
         ##Discription
-        ${description}
+        ${response.description}
 
         ##Installation
-        ${installation}
+        ${response.installation}
 
         ##Usage
-        ${usage}
+        ${response.usage}
 
         ##Contribution
-        ${contribution}
+        ${response.contribution}
 
         ##Tests
-        ${tests}
+        ${response.tests}
 
         ##License
-        ${license}
+        ${response.license}
 
         ##Questions
-        If you have any questions about the repo, open an issue or contact me directly at ${email}.
-        You can find more of my work at my [GitHub](https://github.com/${github})
+        If you have any questions about the repo, open an issue or contact me directly at ${response.email}.
+        You can find more of my work at my [GitHub](https://github.com/${response.github})
 
         `
 }
+
+async function init() {
+    try {
+        const response = await prompts();
+        const readmePageContent = generateREADME(response);
+        // response.title, response.location, response.description, response.installation, response.usage, response.contribution, response.tests, response.license, response.github, response.email
+        fs.writeFile('README.md', readmePageContent, (err) => {
+            err ? console.error(err) : console.log("Created README.md")
+        })
+    } catch (err) { console.log(err) }
+}
+init();
 
 
